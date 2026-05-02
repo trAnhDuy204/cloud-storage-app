7d6b3f96f9b4d8ea8e5ec732ab207a4ff8296fe0
Tran Ha Anh Duy <trananhduydl@gmail.com>
2026-05-03 00:18:18 +0700

    fix globals.css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from "react";
-import './globals.css';
+import './layout/globals.css';
 
 export const metadata: Metadata = {
   title: 'Hellfile',
